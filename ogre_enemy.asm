0314c65140d34a986ed0a305873f8f40ac1d9e91
Luis G. Torres <lgtorres42@gmail.com>
2021-08-13 22:09:03 -0500

    Ogre faces player!

--- ogre_enemy.asm
+++ ogre_enemy.asm
@@ -6,15 +6,41 @@
 ; sp+16: dying_frame_left
 ; sp+18: state
 ;
-; Don't touch d2
+; DON'T TOUCH d2
 DrawOgreEnemy:
+    ; get the appropriate pose based on direction
+    move.l #.DirectionJumpTable,a0
+    clr.l d0
+    move.b 13(sp),d0 ; direction
+    lsl.l #2,d0 ; translate longs into bytes
+    add.l d0,a0
+    ; dereference jump table to get address to jump to
+    move.l (a0),a0
+    jmp (a0)
+    ; TODO: consider making the entries into actual branch instructions.
+    ; Can be 2 cycles faster apparently?
+.DirectionJumpTable dc.l .Up,.Down,.Left,.Right
+.Up
+    move.w #(OGRE_SPRITE_TILE_START+5*36),d1
+    bra.s .AfterDirectionJump
+.Down
+    move.w #OGRE_SPRITE_TILE_START,d1
+    bra.s .AfterDirectionJump
+.Left
+    move.w #OGRE_SPRITE_TILE_START,d1
+    bra.s .AfterDirectionJump
+.Right
+    move.w #(OGRE_SPRITE_TILE_START+5*36),d1
+    bra.s .AfterDirectionJump
+.AfterDirectionJump
+
     move.w #$0A00,d0 ; 3x3 (to be combined with link data)
     add.w #1,SPRITE_COUNTER
     move.b (SPRITE_COUNTER+1),d0 ; link to next sprite
     move.w d0,LAST_LINK_WRITTEN
     move.w 4(sp),d3 ; y
     move.w 8(sp),d4 ; x
-    move.w #OGRE_SPRITE_TILE_START,d1
+    ;move.w #OGRE_SPRITE_TILE_START,d1
     move.w d3,vdp_data ; y
     move.w d0,vdp_data ; link data
     move.w d1,vdp_data ; tile (default palette)
@@ -95,5 +121,33 @@ DrawOgreEnemy:
 ; ENEMY_DATA_1: 0000 0000 0000 0000
 ; ENEMY_DATA_2: 0000 0000 0000 00(direction,2)
 UpdateOgreEnemy:
-    
+    move.w (a3),d0 ; enemy_x
+    sub.w CURRENT_X,d0 ; enemy_x - hero_x
+    jsr AbsValue ; | enemy_x - hero_x |
+    move.w d0,d1
+    move.w (a4),d0 ; enemy_y
+    sub.w CURRENT_Y,d0 ; enemy_y - hero_y
+    jsr AbsValue ; | enemy_y - hero_y |
+    cmp.w d0,d1 ; |dx| - |dy|
+    bgt.s .FacingX
+    ; facing y
+    move.w (a4),d0 ; enemy_y
+    sub.w CURRENT_Y,d0 ; enemy_y - hero_y
+    blt.s .FacingDown
+    ; facing up
+    move.b #FACING_UP,1(a6)
+    bra.s .End
+.FacingDown
+    move.b #FACING_DOWN,1(a6)
+    bra.s .End
+.FacingX
+    move.w (a3),d0 ; enemy_x
+    sub.w CURRENT_X,d0 ; enemy_x - hero_x
+    blt.s .FacingRight
+    ; facing left
+    move.b #FACING_LEFT,1(a6)
+    bra.s .End
+.FacingRight
+    move.b #FACING_RIGHT,1(a6)
+.End
     rts
