8295fb8f925e7dd8fae14d9a01550de70097396c
Luis G. Torres <lgtorres42@gmail.com>
2021-08-24 23:12:54 -0500

    Added script I used to generate slash sprite in previous commit. I should try to unify the 3x3 and 4x4 anim scripts into one mega script maybe

---
+++ aseprite_scripts/export_sprite_anim_4x4.lua
@@ -0,0 +1,48 @@
+-- MD sprites are column-major in how they read tiles
+
+local spr = app.activeSprite
+if not spr  then
+    return app.alert("There is no active sprite")
+end
+
+if spr.colorMode ~= ColorMode.INDEXED then
+    return app.alert("Color mode must be indexed")
+end
+
+-- Let's divvy up into 8x8 cells one at a time, thanks in advance.
+if spr.height % 8 ~= 0 then
+    return app.alert(string.format("height must be a multiple of 8 px (%d px)", image.height))
+end
+if spr.width % 8 ~= 0 then
+    return app.alert(string.format("width must be a multiple of 8 px (%d px)", image.width))
+end
+
+local numTilesX = spr.width / 8
+local numTilesY = spr.height / 8
+for celIx,cel in ipairs(spr.cels) do
+    local image = cel.image
+    for tileY=0,numTilesY-1,4 do
+        for tileX=0,numTilesX-1 do
+            local xPos = tileX*8
+            for ty=tileY,tileY+3 do
+                if ty >= numTilesY then
+                    break
+                end
+                local yPos = ty*8
+                for y=yPos,yPos+7 do
+                    local row = "\tdc.l $"
+                    for x=xPos,xPos+7 do
+                        local xInCel = x - cel.position.x
+                        local yInCel = y - cel.position.y
+                        if xInCel >= 0 and xInCel < image.width and yInCel >= 0 and yInCel < image.height then
+                            row = row .. string.format("%x",image:getPixel(xInCel,yInCel) % 16)
+                        else
+                            row = row .. string.format("0")
+                        end
+                    end
+                    print(row)
+                end
+            end 
+        end
+    end
+end
