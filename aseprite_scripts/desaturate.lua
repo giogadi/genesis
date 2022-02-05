5645052337e3361be78e81c9c57ba1c2e8102d92
Luis G. Torres <lgtorres42@gmail.com>
2022-02-05 15:19:43 -0600

    Generalize some aseprite scripts to specify which palette ranges to use for input and output.

--- aseprite_scripts/desaturate.lua
+++ aseprite_scripts/desaturate.lua
@@ -3,17 +3,21 @@ if not spr then
   return app.alert("There is no active sprite")
 end
 
+local INPUT_START = 32
+local COUNT = 16
+local OUTPUT_START = 32
+
 -- Each Palette:setColor() call will be grouped in one undoable
 -- transaction. In this way the Edit > Undo History will contain only
 -- one item that can be undone.
 app.transaction(
   function()
     local pal = spr.palettes[1]
-    for i = 0,#pal-1 do
+    for i = 0,COUNT-1
       -- Want to do color & 0xF0. dividing out and re-multiplying does the trick.
-      local color = pal:getColor(i)
+      local color = pal:getColor(INPUT_START+i)
       color.saturation = color.saturation / 2;
-      pal:setColor(i, color);
+      pal:setColor(OUTPUT_START+i, color);
     end
   end)
 
--- aseprite_scripts/invert-colors.lua
+++ aseprite_scripts/invert-colors.lua
@@ -3,18 +3,23 @@ if not spr then
   return app.alert("There is no active sprite")
 end
 
+local INPUT_START = 0
+local COUNT = 16
+local OUTPUT_START = 32
+
 -- Each Palette:setColor() call will be grouped in one undoable
 -- transaction. In this way the Edit > Undo History will contain only
 -- one item that can be undone.
 app.transaction(
   function()
     local pal = spr.palettes[1]
-    for i = 0,#pal-1 do
+    for i = 0,COUNT-1
       -- Want to do color & 0xF0. dividing out and re-multiplying does the trick.
-      local color = pal:getColor(i)
-      pal:setColor(i, Color{ r=255 - color.red,
-                             g=255 - color.green,
-                             b=255 - color.blue })
+      local color = pal:getColor(INPUT_START+i)
+      pal:setColor(OUTPUT_START+i,
+        Color{ r=255 - color.red,
+               g=255 - color.green,
+               b=255 - color.blue })
     end
   end)
 
